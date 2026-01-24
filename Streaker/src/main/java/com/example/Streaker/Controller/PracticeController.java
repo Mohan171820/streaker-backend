e7dce1c35f17a5a9216cdac218450848a9e90170
Mohan Murali <miriyalamohanmurali74@gmail.com>
2026-01-24 12:19:23 +0530

    Added the versioning to the controllers

--- Streaker/src/main/java/com/example/Streaker/Controller/PracticeController.java
+++ Streaker/src/main/java/com/example/Streaker/Controller/PracticeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/practice")
+@RequestMapping("/api/v1/practice")
 @RequiredArgsConstructor
 public class PracticeController {
 
--- Streaker/src/main/java/com/example/Streaker/Controller/SkillController.java
+++ Streaker/src/main/java/com/example/Streaker/Controller/SkillController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/skills")
+@RequestMapping("/api/v1/skills")
 @RequiredArgsConstructor
 public class SkillController {
 

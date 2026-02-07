008f92ab9b301e7dcdb6d36f74ed0e4193a30673
Mohan Murali <miriyalamohanmurali74@gmail.com>
2026-02-07 21:11:46 +0530

    Implemented the Acutator

--- target/generated-sources/annotations/com/example/Streaker/Mapper/PracticeMapperImpl.java
+++ target/generated-sources/annotations/com/example/Streaker/Mapper/PracticeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-23T18:21:18+0530",
+    date = "2026-02-07T21:08:46+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
